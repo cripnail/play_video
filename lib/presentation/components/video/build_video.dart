d3d557bd44fa0581a4b76c7c4293a30561cf5214
Nail Sharipov <nikorurk@icloud.com>
2022-11-17 00:32:21 +0500

    Move PlayControl() from lib/presentation/components/video/build_bar.dart to lib/presentation/components/video/build_video.dart

--- lib/presentation/components/video/build_video.dart
+++ lib/presentation/components/video/build_video.dart
@@ -3,16 +3,18 @@ import 'package:media_player/presentation/components/video/video_controls.dart';
 import 'package:video_player/video_player.dart';
 
 import '../../../bloc/video_state.dart';
+import 'controls/play_control.dart';
 
 class BuildVideo extends StatelessWidget {
   const BuildVideo({
     super.key,
     required this.state,
-    required this.timestamps,
+    required this.timestamps, required this.iconSize,
   });
 
   final VideoState state;
   final List<Duration> timestamps;
+  final double iconSize;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +23,9 @@ class BuildVideo extends StatelessWidget {
       children: [
         VideoPlayer(
           state.controller,
+        ),
+        PlayControl(
+          iconSize: iconSize,
         ),
         VideoControls(
           state.controller,
--- lib/presentation/components/video/video.dart
+++ lib/presentation/components/video/video.dart
@@ -6,36 +6,16 @@ import '../../../bloc/video_state.dart';
 import 'build_video.dart';
 
 class Video extends StatelessWidget {
-
   const Video._(
     this.url, {
     Key? key,
     required this.aspectRatio,
+    required this.iconSize,
   }) : super(key: key);
-
-  static Widget blocProvider(
-    String url, {
-    required double aspectRatio,
-    bool autoPlay = true,
-    bool? controlsVisible,
-  }) {
-    return BlocProvider(
-      create: (_) {
-        return VideoCubit(
-          url,
-          autoPlay: autoPlay,
-          controlsVisible: controlsVisible ?? !autoPlay,
-        );
-      },
-      child: Video._(
-        url,
-        aspectRatio: aspectRatio,
-      ),
-    );
-  }
 
   final String url;
   final double aspectRatio;
+  final double iconSize;
 
   @override
   Widget build(
@@ -50,10 +30,59 @@ class Video extends StatelessWidget {
             aspectRatio: aspectRatio,
             child: state.notLoaded
                 ? const Center(child: CircularProgressIndicator())
-                : BuildVideo(state: state, timestamps: [],),
+                : BuildVideo(
+                    state: state,
+                    timestamps: const [],
+                    iconSize: iconSize,
+                  ),
           ),
         );
       },
+    );
+  }
+
+  static blocProvider(String s,
+      {required double aspectRatio, required bool autoPlay}) {}
+}
+
+class BlocProvider extends StatelessWidget {
+  const BlocProvider({
+    super.key,
+    required this.iconSize,
+    required this.url,
+    required this.aspectRatio,
+    required this.autoPlay,
+    required this.controlsVisible,
+    required Video child,
+    required VideoCubit Function(dynamic _) create,
+  });
+
+  final double iconSize;
+  final String url;
+  final double aspectRatio;
+  final bool autoPlay;
+  final bool? controlsVisible;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) {
+        return VideoCubit(
+          url,
+          autoPlay: autoPlay,
+          controlsVisible: controlsVisible ?? !autoPlay,
+        );
+      },
+      iconSize: iconSize,
+      url: '',
+      aspectRatio: aspectRatio,
+      autoPlay: autoPlay,
+      controlsVisible: controlsVisible,
+      child: Video._(
+        url,
+        aspectRatio: aspectRatio,
+        iconSize: iconSize,
+      ),
     );
   }
 }
