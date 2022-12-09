134279e279897b72be4e2c4537b06f237575ba5e
Nail Sharipov <nikorurk@icloud.com>
2022-12-09 20:30:06 +0500

    Fix project launch on iOS & MacOS platforms

--- lib/presentation/components/video/build_bar.dart
+++ lib/presentation/components/video/build_bar.dart
@@ -3,7 +3,6 @@ import 'package:video_player/video_player.dart';
 import '../../../bloc/video_cubit.dart';
 import 'controls/current_video_position.dart';
 import 'controls/custom_control_widget.dart';
-import 'controls/play_control.dart';
 import 'controls/progress_indicator_control.dart';
 
 class BuildBar extends StatelessWidget {
@@ -24,35 +23,25 @@ class BuildBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black38,
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 4.0,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  PlayControl(
-                    iconSize: iconSize,
-                  ),
-                  CustomControlsWidget(
-                    controller: controller,
-                    timestamps: timestamps,
-                  ),
-                ],
-              ),
-            ),
+    return Column(children: [
+      Expanded(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 4.0,
           ),
-          ProgressIndicatorControl(controller: controller),
-          CurrentVideoPosition(videoPlayerController: controller),
-        ],
+          child: Column(
+            children: [
+              CustomControlsWidget(
+                controller: controller,
+                timestamps: timestamps,
+              ),
+            ],
+          ),
+        ),
       ),
-    );
+      ProgressIndicatorControl(controller: controller),
+      CurrentVideoPosition(videoPlayerController: controller),
+    ]);
   }
 }
